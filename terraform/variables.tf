7dc861a384cd9f8573486a336d74f6ba2b4bec85
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-15 14:50:39 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,24 @@
+variable "subscription_id" {
+  description = "The Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Client ID of the Azure Service Principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Azure Service Principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+  type        = string
+}
+
 variable "acr_name" {
   description = "Azure Container Registry name (must be globally unique)"
   default     = "dotnetacr-rudram123"
