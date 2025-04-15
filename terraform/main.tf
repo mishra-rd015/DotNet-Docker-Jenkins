b233188c0c4028c0c265721fec827609173f3e0c
Rudram Mishra <rudrammishra15@gmail.com>
2025-04-15 12:34:36 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "kube"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "dotnet-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name (must be globally unique)"
+  default     = "dotnetacr123"
+}
+
+variable "aks_name" {
+  description = "Azure Kubernetes Service name"
+  default     = "dotnet-aks"
+}
