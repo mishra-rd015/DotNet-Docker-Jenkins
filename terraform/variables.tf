29dadcf5eda036842885887ee3b31c9d84089c50
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-15 14:16:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "acr_name" {
-  default = "dotnetacr-rudram123"  
+  description = "Azure Container Registry name (must be globally unique)"
+  default     = "dotnetacr-rudram123"
 }
 
 variable "resource_group_name" {
@@ -10,11 +11,6 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region"
   default     = "East US"
-}
-
-variable "acr_name" {
-  description = "Azure Container Registry name (must be globally unique)"
-  default     = "dotnetacr123"
 }
 
 variable "aks_name" {
